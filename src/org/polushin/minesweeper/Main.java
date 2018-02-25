11f2d3263f96ad685a010643f38dfefab5a1b488
Kirill Polushin <kirill@polushin.org>
2018-02-25 21:36:29 +0700

    Added command line options.

--- src/org/polushin/minesweeper/Main.java
+++ src/org/polushin/minesweeper/Main.java
@@ -1,18 +1,32 @@
 package org.polushin.minesweeper;
 
 import org.polushin.minesweeper.cui.TextUI;
+import org.polushin.minesweeper.gui.MainFrame;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		//new MainFrame(new File("scores.json"));
-		try {
-			new TextUI(new File("scores.json")).runGame();
-		} catch (IOException e) {
-			e.printStackTrace();
+	private static final File SCORES_FILE = new File("scores.json");
+
+	public static void main(String[] args) throws IOException {
+		if (args.length < 1) {
+			System.out.println("No args, run GUI...");
+			new MainFrame(SCORES_FILE);
+			return;
+		}
+
+		switch (args[0]) {
+			case "gui":
+				new MainFrame(SCORES_FILE);
+				break;
+			case "cui":
+				new TextUI(SCORES_FILE).runGame();
+				break;
+			default:
+				System.out.println("Need arg: <gui/cui>");
+				break;
 		}
 	}
 }
